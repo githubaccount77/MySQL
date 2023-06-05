38ae85bd74b16aaab6be0d06d94ff5b23b1e8339
githubaccount77 <christophertubman@outlook.com>
2023-06-05 12:30:07 -0400

    CT Changelog 6/5/23

---
+++ myDB.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees
+VALUES (2, "Christopher", "Walker", 25.50, "1992-08-16"),
+(3, "Alexander", "MacDonald", 25.50, "2023-01-23"),
+(4, "John", "Morrison", 25.50, "2023-01-22"),
+(5, "John", "Walker", 25.50, "2023-01-20");
+
+SELECT * FROM employees;
